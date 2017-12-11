122e3e437c5937ae31d788d9eeb5259cd33d98db
anis25 <aniscseru6125@gmail.com>
2017-12-11 19:18:06 +0600

    Now my program working properly...

--- src/GladLib.java
+++ src/GladLib.java
@@ -41,10 +41,67 @@ public class GladLib {
         for(int i=0; i<9; i++)
             myMap.put(category[i], readIt(source + "/" + category[i] + ".txt"));
     }
-    
+
     private String randomFrom(ArrayList<String> list){
         int index = myRandom.nextInt(list.size());
         return list.get(index);
     }
 
+    private String getSubstitute(String label){
+        if(label.equals("number"))
+            return Integer.toString(myRandom.nextInt(100));
+        if(!myMap.containsKey(label))
+            return "**Unknown**";
+        return randomFrom(myMap.get(label));
+    }
+
+    private String processWord(String str){
+        int start = str.indexOf('<');
+        int end = str.indexOf('>');
+        if(start == -1 || end == -1)
+            return str;
+        String prefix = str.substring(0, start);
+        String suffix = str.substring(end+1);
+        String mid = getSubstitute(str.substring(start+1, end));
+        return prefix + mid + suffix;
+    }
+
+    private String fromTemplate(String source){
+        String story = "";
+        if(source.startsWith("http")){
+            URLResource urlResource = new URLResource(source);
+            for(String word : urlResource.words())
+                System.out.println(word);
+        }
+
+        else{
+            FileResource fileResource = new FileResource(source);
+            for(String word : fileResource.words())
+                story += processWord(word) + " ";
+        }
+        return story;
+    }
+
+    private void printOut(String str, int lineBreak){
+        int count = 0;
+        for(String word : str.split("\\s+")){
+            if(count + word.length() > lineBreak){
+                System.out.println();
+                count = 0;
+            }
+            System.out.print(word + " ");
+            count += word.length() + 1;
+        }
+    }
+
+    public void makeStory(){
+        String story = fromTemplate(dataSourceURL + "/madtemplate.txt");
+        printOut(story, 70);
+    }
+
+    public static void main(String args[]){
+        GladLib obj = new GladLib();
+        obj.makeStory();
+    }
 }
+
