48d988a9d65c0724c8f062db48ec5e06b5683831
anis25 <aniscseru6125@gmail.com>
2017-12-11 16:29:08 +0600

    There is only one method called 'addWordsFromFile()' that add the words to the hashmap.....

---
+++ src/WordsInFiles.java
@@ -0,0 +1,82 @@
+import edu.duke.DirectoryResource;
+import edu.duke.FileResource;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.*;
+
+public class WordsInFiles {
+   private Map<String,ArrayList<String>> mymap;
+   public ArrayList<String> abb = new ArrayList<String>();
+   WordsInFiles()
+   {
+       mymap = new HashMap<String,ArrayList<String>>();
+
+   }
+
+   private void addWordsFromFile(File f)
+   {
+       String filename = f.getName();
+       FileResource fr = new FileResource(f);
+       for(String line: fr.lines())
+       {
+          String word = "";
+          for(int i = 0; i < line.length(); i++)
+          {
+
+              if(Character.isLetter(line.charAt(i)))
+              {
+                 word+=line.charAt(i);
+              }
+             else
+             {
+                 if(word.length() > 0)
+                 {
+
+                     if(mymap.containsKey(word))
+                     {
+                         if(mymap.get(word).indexOf(filename) == -1)
+                         {
+                             ArrayList<String> val = new ArrayList<String>();
+                             val = mymap.get(word);
+                             val.add(filename);
+                             mymap.put(word,val);
+                         }
+                     }
+                     else
+                     {
+                         ArrayList<String> val = new ArrayList<String>();
+                         val.add(filename);
+                         mymap.put(word,val);
+                     }
+                 }
+
+                 word = "";
+             }
+          }
+
+          if(word.length() > 0)
+          {
+              if(mymap.containsKey(word))
+              {
+
+                  if(mymap.get(word).indexOf(filename) == -1)
+                  {
+                      ArrayList<String> val = new ArrayList<String>();
+                      val = mymap.get(word);
+                      val.add(filename);
+                      mymap.put(word,val);
+                  }
+              }
+              else
+              {
+                  ArrayList<String> val = new ArrayList<String>();
+                  val.add(filename);
+                  mymap.put(word,val);
+              }
+          }
+       }
+   }
+}
